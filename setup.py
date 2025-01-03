76550ace7986ea6842bd57b6427cc364d272702e
Kristupas <kr.jakubonis@gmail.com>
2025-01-03 13:02:27 +0000

    Code tuning

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deep-text-recognition-benchmark',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List dependencies here, if known
+    ],
+)
